4e3da4ec1191eae4c705a6aeb4ceff9f9d67d758
nada <nadawaled87@gmail.com>
2023-03-16 18:34:39 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
