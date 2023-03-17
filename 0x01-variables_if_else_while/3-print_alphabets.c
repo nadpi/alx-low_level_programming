0dd729c93f71bcae2e2c7d9ee44134838d0ed638
nada <nadawaled87@gmail.com>
2023-03-17 08:58:37 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,21 +8,19 @@ int main(void)
 {
 	char lowalphabet = 'a';
 	char upalphabet = 'A';
-        int i;
+	int i;
 	int j;
 
 	for (i = 0 ; i < 26 ; i++)
-        {
-                putchar(lowalphabet);
-                lowalphabet++;
-        }
-
-        for (j = 0; j < 26 ; j++)
+	{
+		putchar(lowalphabet);
+		lowalphabet++;
+	}
+	for (j = 0; j < 26 ; j++)
 	{
 		putchar(upalphabet);
 		upalphabet++;
 	}
-
 	putchar('\n');
 	return (0);
 }
