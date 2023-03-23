dad152b23590993422659c38bcbcd51f6825d377
nada <nadawaled87@gmail.com>
2023-03-22 20:13:29 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,10 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	if(num < 0)
-        {
-                num = num * -1;
-        }
+	if (num < 0)
+	{
+		num = num * -1;
+	}
 
 	last_digit = num % 10;
 	
