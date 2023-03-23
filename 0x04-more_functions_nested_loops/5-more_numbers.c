13d0279364f02f0c10905f0dce5ac620837c41e1
nada <nadawaled87@gmail.com>
2023-03-23 16:28:06 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-		       	if (j >= 10)
+			if (j >= 10)
 			{
 				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
 			}
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
