18a32bcd44164e3950d36e44e053d6667a238c49
nada <nadawaled87@gmail.com>
2023-03-23 15:41:57 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: printed numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
