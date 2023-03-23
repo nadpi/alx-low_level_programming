ae9eec9cbeb0a1e6c879d524f172bd247086df09
nada <nadawaled87@gmail.com>
2023-03-22 19:03:34 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute of a number
+ * @num: type int parameter
+ * Return: the absolute number
+ */
+int _abs(int num)
+{
+	num = abs(num);
+	return (num);
+}
