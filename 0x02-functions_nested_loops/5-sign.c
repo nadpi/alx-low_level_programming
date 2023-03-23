3268b9907162d61f61c029a6fe6c135c37e84d33
nada <nadawaled87@gmail.com>
2023-03-22 18:52:11 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of the number
+ * @n: int type parameter
+ * Return: the sign.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
