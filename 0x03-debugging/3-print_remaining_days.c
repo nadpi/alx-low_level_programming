730cf9ec597372a0239c94809ffe618035de1750
nada <nadawaled87@gmail.com>
2023-03-23 19:20:01 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,8 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 61 && year % 4 == 0 ||
-			month == 2 && day == 61 && year % 4 != 0)
+		if (month == 2 && day == 61 && year % 4 == 0)
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
