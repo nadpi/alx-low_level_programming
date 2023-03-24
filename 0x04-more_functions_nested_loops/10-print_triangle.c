67e9a817e002a64cf77886755445e63f908810dd
nada <nadawaled87@gmail.com>
2023-03-23 18:56:01 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,9 +8,10 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if(size > 0){
 	for (i = 0; i < size; i++)
 	{
-		for (j = size-1 ; j > i; j--)
+		for (j = size - 1 ; j > i; j--)
 		{
 			_putchar(' ');
 
@@ -19,6 +20,11 @@ void print_triangle(int size)
 		{
 		_putchar('#');
 		}
+		_putchar('\n');
+	}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
