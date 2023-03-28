2000cc9f7a755d8304ba70c1371ec6dca6fc2299
nada <nadawaled87@gmail.com>
2023-03-27 17:14:54 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string
+ * Return: needed output
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i+=2;
+	}
+	printf("\n");
+}
