e893a661510514cf58cdbffe629dec5fffc4c095
nada <nadawaled87@gmail.com>
2023-03-27 19:11:39 -0700

    fixes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0, length = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[length]);
+			_putchar(str[i]);
 			i++;
 		}
 		else
