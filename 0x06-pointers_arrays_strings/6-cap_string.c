d8bd885651f3bfe6fc8cb35cd16925f207cfa545
nada <nadawaled87@gmail.com>
2023-03-29 14:44:11 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string
+ * Return: the result
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if(s[i] == ' ' || s[i] == '\n' || s[i] == '"'
+			|| s[i] == '(' || s[i] == '{' || 
+			s[i] == '\t' || s[i] == '.')
+		{
+			if(s[i + 1] >= 97){
+			s[i+1] = s[i+1] - 32;
+			i++;
+			}
+			else
+				i++;
+		}
+		else if ((s[i] == ',' && s[i + 1] == ' ') || 
+			(s[i] == ';' && s[i + 1] == ' ') ||
+			(s[i] == '.' && s[i + 1] == ' ') ||
+			(s[i] == '!' && s[i + 1] == ' ') ||
+			(s[i] == '?' && s[i + 1] == ' ') ||
+			(s[i] == '"' && s[i + 1] == ' ') ||
+			(s[i] == ')' && s[i + 1] == ' ') ||
+			(s[i] == '}' && s[i + 1] == ' '))
+		{
+			/*s[i+2] = s[i+2] - 32;*/
+			i++;
+		}
+		else
+			i++;
+	}
+	return (s);
+}
