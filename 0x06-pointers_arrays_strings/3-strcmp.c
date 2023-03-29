8f53341fe03c65acf8f4da85625e44b7f7d07a55
nada <nadawaled87@gmail.com>
2023-03-29 12:39:14 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: string1
+ * @s2: string2
+ * Return: -1 , 0 , 1 (less than, equal, bigger than)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+		}
+		else
+		{
+			j = s1[i] - s2[j];
+			return (j);
+		}
+	}
+	return (0);
+}
