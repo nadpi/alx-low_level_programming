d5c54a9f1bea3e5f5a1f116a9ae8cb36501f40af
nada <nadawaled87@gmail.com>
2023-03-31 15:41:53 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,14 +5,16 @@
  */
 char *leet(char *s)
 {
-	char alph[] = {'a' , 'e' , 'o', 't', 'l'};
+	char alph[] = {'a', 'e', 'o', 't', 'l'};
 	char capalph[] = {'A', 'E', 'O', 'T', 'L'};
-	char leet[] = {'4', '3' , '0' , '7', '1'};
+	char leet[] = {'4', '3', '0', '7, '1'};
+
 	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		int j = 0;
+
 		while (j < 5)
 		{
 			if (s[i] == alph[j] || s[i] == capalph[j])
