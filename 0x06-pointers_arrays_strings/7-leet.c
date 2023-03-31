619a6f80c12af1e253a6a74ace42e77c2e0e92b4
nada <nadawaled87@gmail.com>
2023-03-31 15:38:31 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+/**
+ * leet - encodes string to 1337
+ * @s: string
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	char alph[] = {'a' , 'e' , 'o', 't', 'l'};
+	char capalph[] = {'A', 'E', 'O', 'T', 'L'};
+	char leet[] = {'4', '3' , '0' , '7', '1'};
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		int j = 0;
+		while (j < 5)
+		{
+			if (s[i] == alph[j] || s[i] == capalph[j])
+				{
+					s[i] = leet[j];
+					j++;
+				}
+			else
+				j++;
+		}
+		i++;
+	}
+	return (s);
+}
