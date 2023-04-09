b9a79bca54d9969f9836e24ad17a262a4dde835b
nada <nadawaled87@gmail.com>
2023-04-09 15:13:44 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: no. of argms
+ * @argv: argument vector
+ * Return: return 0 (Success)
+ */
+int main(int argc ,char *argv[])
+{
+	printf("%s\n",argv[argc-1]);
+	return (0);
+}
