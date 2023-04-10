09e88e1dfedcfdaf69304c1f165c2358954f9651
nada <nadawaled87@gmail.com>
2023-04-09 18:15:24 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: args count
+ * @argv: args vector
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
