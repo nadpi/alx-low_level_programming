f85135e92ba13fe534f7926744e8312e6ff35c02
nada <nadawaled87@gmail.com>
2023-04-18 05:51:31 -0700

    edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,11 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		ar[i] = malloc(width * sizeof(int));
+		if (ar[i] == NULL)
+		{
+			free(ar[i]);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
