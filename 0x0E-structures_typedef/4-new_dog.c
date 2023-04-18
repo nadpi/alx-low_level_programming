8ef8d180527c81655f98402b1de28bf0fed0df47
nada <nadawaled87@gmail.com>
2023-04-17 19:17:07 -0700

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newdog;
 
 	newdog = &d;
-	if (newdog->name && newdog->owner)
+	if (newdog)
 	{
 	newdog->name = name;
 	newdog->age = age;
