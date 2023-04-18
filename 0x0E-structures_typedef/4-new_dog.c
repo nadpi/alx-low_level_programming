bfcc4c26ad3c2450ac129c5da00c9418a63f472f
nada <nadawaled87@gmail.com>
2023-04-17 18:26:18 -0700

    task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * new_dog -  creates a new dog
  * @name: name of dog
@@ -11,6 +12,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog d;
 	struct dog *newdog;
 
+	newdog = malloc(sizeof(struct dog));
+	if (newdog == NULL)
+		return (NULL);
 	newdog = &d;
 	newdog->name = name;
 	newdog->age = age;
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: dog struct
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
