5acdb1453277e61f9f9a66476e2927dea87dec72
nada <nadawaled87@gmail.com>
2023-04-18 06:11:42 -0700

    ed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-		if (!isdigit(argv[i][j]))
+		if (isalpha(argv[i][j]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-			sum += atoi(argv[i]);
 	}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	}
