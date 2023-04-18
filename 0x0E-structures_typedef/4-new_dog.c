224703e81231e045960421f320af3dc6b05351f8
nada <nadawaled87@gmail.com>
2023-04-18 04:43:37 -0700

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,44 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * _strcpy - copies a string from src to dest
+ * @src: source
+ * @dest: destination
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		if (j == i)
+		{
+			dest[j] = '\0';
+			break;
+		}
+		dest[j] = src[j];
+	}
+	return (dest);
+}
+/**
  * new_dog -  creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -9,14 +47,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
 	dog_t *newdog;
 
-	newdog = &d;
-	if (!newdog)
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+	newdog->name = malloc(sizeof(char) * _strlen(name));
+	newdog->owner = malloc(sizeof(char) * _strlen(owner));
+	_strcpy(newdog->name, name);
+	_strcpy(newdog->owner, owner);
 	newdog->age = age;
-	newdog->owner = owner;
 	return (newdog);
 }
