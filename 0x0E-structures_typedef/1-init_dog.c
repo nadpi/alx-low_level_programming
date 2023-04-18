84f426ae1f274da4c96a70bc8240b3b76b4e6d47
nada <nadawaled87@gmail.com>
2023-04-17 17:33:16 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
