2c70d093349e81c49d4812ead5941632767bca48
nada <nadawaled87@gmail.com>
2023-04-18 05:42:10 -0700

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,5 @@
 #include <stdlib.h>
 /**
- * _strcpy - copies a string from src to dest
- * @src: source
- * @dest: destination
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0, j;
-
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	for (j = 0; j <= i; j++)
-	{
-		if (j == i)
-		{
-			dest[j] = '\0';
-			break;
-		}
-		dest[j] = src[j];
-	}
-	return (dest);
-}
-/**
  * string_nconcat - concatenates two strings.
  * @s1: string 1
  * @s2: string 2
@@ -36,14 +11,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ar;
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 
-	if (s1 == NULL)
-		_strcpy(s1, "");
-	if (s2 == NULL)
-		_strcpy(s2, "");
 	while (s1[i] != '\0' && s1 != NULL)
 		i++;
 	while (s2[j] != '\0' && s2 != NULL)
 		j++;
+
 	if (n >= j)
 		n = j;
 	ar = malloc((i + n + 1) * sizeof(char));
