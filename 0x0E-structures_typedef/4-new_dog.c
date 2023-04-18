d9c80c50f60d050f575af17fa68e34af28579272
nada <nadawaled87@gmail.com>
2023-04-17 18:01:55 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * new_dog -  creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t *newdog;
+
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+	return (newdog);
+}
