81f0a6bab0ce3aa2ebf08cda3ded746f16f4dd72
nada <nadawaled87@gmail.com>
2023-04-19 06:58:37 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 /**
  * array_iterator - executes a function given
  *  as a parameter on each element of an array.
@@ -10,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 int i;
+	long unsigned int i;
 
 	if (!action)
 		return;
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef __mainh__
 #define __mainh__
+#include <stddef.h>
 void _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
