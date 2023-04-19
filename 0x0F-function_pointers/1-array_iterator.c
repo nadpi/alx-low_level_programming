e51d2d22c3c96e8fde7a659147fc93a300214d29
nada <nadawaled87@gmail.com>
2023-04-19 06:59:47 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned int i;
 
-	if (!action)
+	if (!action || !array)
 		return;
 
 	for (i = 0; i < size; i++)
