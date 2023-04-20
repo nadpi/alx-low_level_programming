f69ab4b18b0e65cfb87d9195ae770417ca2dd19a
nada <nadawaled87@gmail.com>
2023-04-20 04:43:24 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (n == 0)
-		return (0);
 	for (i = 0; i < n; i++)
 	{
 		int num = va_arg(args, int);
