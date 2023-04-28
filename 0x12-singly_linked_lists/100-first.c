020c80d2670716417ad17a63922fea522c5492ab
nada <nadawaled87@gmail.com>
2023-04-28 03:23:19 -0700

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * myStartupFun - executes before main
+ * Return: nothing
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
