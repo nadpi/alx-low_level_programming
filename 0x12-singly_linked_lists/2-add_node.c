678bffc4a627c07d43d55dca496d9b7cd24868b1
nada <nadawaled87@gmail.com>
2023-04-27 18:26:48 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,10 +8,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
+	unsigned int i = 0;
+	while (str[i] != '\0')
+		i++;
 
 	if (!newnode)
 		return (NULL);
 	newnode->str = strdup(str);
+	newnode->len = i;
 	newnode->next = *head;
 	*head = newnode;
 	return (newnode);
