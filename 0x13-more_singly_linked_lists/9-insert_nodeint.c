d24e080f9121847bf86d4717dc14bb330a458420
nada <nadawaled87@gmail.com>
2023-05-03 14:19:20 -0700

    edit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newnode->n = n;
 
-	if (!temp)
+	if (idx == 0)
+	{
+		newnode->next = *head;
 		*head = newnode;
+	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
