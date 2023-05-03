8b311fe3e8675da0e342eb22394b4c062a35505e
nada <nadawaled87@gmail.com>
2023-05-03 13:37:05 -0700

    edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *tmp = *head;
 
-	while (head)
+	while (tmp)
 	{
 		temp = tmp;
 		tmp = tmp->next;
 		free(temp);
 	}
-	free(head);
+	free(tmp);
 	tmp = NULL;
 }
