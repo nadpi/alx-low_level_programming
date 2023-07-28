dae20e15d5833115e03574078824c12f96cd7e4c
nada <nadawaled87@gmail.com>
2023-07-28 15:33:52 -0700

    task 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask = 1, i = 0, num = *n;
+    unsigned long int mask = 1;
 
     mask = mask << index;
     *n = *n | mask;
