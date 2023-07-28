41ed0f091fd62ab2a494b2cc956ce8f4684cd023
nada <nadawaled87@gmail.com>
2023-07-28 14:18:05 -0700

    task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,15 +7,17 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit = 0, i = 0;
-	unsigned long int num = n;
+	int bit = 0;
+	unsigned long int num = n, i = 0;
 
 	while (n >> i)
 	{
 		i++;
 	}
+	if (index > i)
+		return (-1);
 	index = i - index;
-	index = i - index;
+	index = i - index ;
 	bit = (num >> index) & 1;
 	return (bit);
 }
