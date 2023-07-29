80f7b3d6f9ffb0d74feb47596c0bee9b5ecf89bb
nada <nadawaled87@gmail.com>
2023-07-28 20:00:06 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, mode);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 
 	if (fp == -1)
 		return (-1);
 
+	if (text_content != NULL)
+	{
 	letters = write(fp, text_content, _strlen(text_content));
 
 	if (letters == -1)
-		return (0);
-
+		return (-1);
+	}
 	close(fp);
-	return (letters);
+	return (1);
 }
 /**
  * _strlen - returns the length of a string
