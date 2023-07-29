3fe5f47fe64fe43b34a21f001579cbd2e08e6732
nada <nadawaled87@gmail.com>
2023-07-28 19:54:43 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: filename
+ * @text_content: string
+ * Return: 1 / -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	mode_t mode = S_IRUSR | S_IWUSR;
+	int fp;
+	ssize_t letters;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, mode);
+
+	if (fp == -1)
+		return (-1);
+
+	letters = write(fp, text_content, _strlen(text_content));
+
+	if (letters == -1)
+		return (0);
+
+	close(fp);
+	return (letters);
+}
---
+++ 0x15-file_io/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
