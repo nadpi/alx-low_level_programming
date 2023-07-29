14fad48e3af43d3542e0e641ec2f6922bece2781
nada <nadawaled87@gmail.com>
2023-07-29 02:22:27 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	letters = write(fp, text_content, sizeof(text_content));
+	letters = write(fp, text_content, _strlen(text_content));
 
 	if (letters == -1)
 		return (-1);
