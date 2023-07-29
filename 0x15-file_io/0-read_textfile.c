0bdb2a9b028978b193a667d9be565789593fa89c
nada <nadawaled87@gmail.com>
2023-07-28 19:31:45 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,34 +9,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp;
 	char *c = malloc(letters * sizeof(char));
-	ssize_t i = 0, readd;;
+	ssize_t i = 0, readd;
 
 	if (c == NULL)
-	{
 		return (0);
-	}
 
 	if (filename == NULL)
 		return (0);
 
 	fp = open(filename, O_RDONLY);
-	
+
 	if (fp == -1)
 		return (0);
 
 	i = read(fp, c, letters);
-	c[i] = '\0';
-	 if (i <= 0)
-	 {
-		 free(c);
-		 return (0);
-	 }
 
-	 readd = write(STDOUT_FILENO, c, i);
-	 
-	 if (readd != i)
-		 return (0);
+	if (i <= 0)
+	{
+		free(c);
+		return (0);
+	}
 
-	 close(fp);
+	readd = write(STDOUT_FILENO, c, i);
+
+	if (readd != i)
+		return (0);
+
+	close(fp);
 	return (readd);
 }
