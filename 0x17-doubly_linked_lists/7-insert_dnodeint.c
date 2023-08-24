5868b38951184d0316a245f8398bd8c4216f5fcc
nada <nadawaled87@gmail.com>
2023-08-23 18:38:16 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	newnode->n = n;
-	if (!h || idx == 0)
+	if (idx == 0)
 	{
 		newnode->next = curr;
 		curr->prev = newnode;
