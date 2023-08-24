8333319d64f9b0e17fb6090295161e393b0a1730
nada <nadawaled87@gmail.com>
2023-08-23 18:02:07 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sumsall the data of list
+ * @h: list
+ * Return: the sum of nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	const dlistint_t *curr = head;
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (curr != NULL)
+	{
+		sum += curr->n;
+		curr = curr->next;
+	}
+	return (sum);
+}
