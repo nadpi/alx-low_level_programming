8dfcf97156067697735866459903e078b71a4419
nada <nadawaled87@gmail.com>
2023-09-26 16:01:34 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hashT->size = size;
 
-	hashT->array = malloc(sizeof(hash_node_t) * size);
+	hashT->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hashT->array)
 		return (NULL);
