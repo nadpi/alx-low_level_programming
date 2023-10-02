7dbeb0b8ad6c35035b413ec365928e6af8ee9828
nada <nadawaled87@gmail.com>
2023-10-02 07:29:48 -0700

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p, *next;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			next = p->next;
+			free(p->value);
+			free(p->key);
+			free(p);
+			p = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
