f2c0c54f5290d4dbfa8e8b7338afff0476610543
nada <nadawaled87@gmail.com>
2023-10-02 05:53:15 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[idx] = newnode;
 	else
 	{
-		ht->array[idx] = newnode;
+		ht->array[idx]->next = newnode;
 	}
 	return (1);
 }
