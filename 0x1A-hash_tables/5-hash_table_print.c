4cab071aed8a80be08601b7b174eda4502b72dca
nada <nadawaled87@gmail.com>
2023-10-02 07:08:10 -0700

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,26 +7,34 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, cnt = 0;
+	hash_node_t *p;
 
 	if (!ht)
-		exit (1);
+		exit(1);
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+	p = ht->array[i];
+		while (p)
+		{
 			cnt++;
+			p = p->next;
+		}
 	}
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		p = ht->array[i];
+		while (p)
 		{
-		printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
-		if (cnt > 1)
-		{
-		printf(", ");
-		cnt--;
-		}
+			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (cnt > 1)
+			{
+				printf(", ");
+				cnt--;
+			}
+			p = p->next;
 		}
 	}
 	printf("}\n");
