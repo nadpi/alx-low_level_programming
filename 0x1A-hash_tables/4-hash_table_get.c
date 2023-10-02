e3f353dcaf1132934c8579dabd6f7d3c7c692632
nada <nadawaled87@gmail.com>
2023-10-02 06:30:44 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	else
 	{
-		if (strcmp(item->key, key) == 0)
+		if (strcmp(ht->array[idx]->key, key) == 0)
 			return (ht->array[idx]->value);
 	}
 }
