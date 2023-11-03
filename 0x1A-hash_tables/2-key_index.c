23bef0e1aacd5e8dc3bece3be92625351684f7b6
nadpi <nadawaled87@gmail.com>
2023-11-03 16:06:45 -0700

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,7 @@
  * key_index - a function that gives you the index of a key.
  * @key: key
  * @size: size
+ * Return: key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
