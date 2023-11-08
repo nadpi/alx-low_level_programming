df20f2033e0b478cf012d0ff0096f9addfde313a
nadpi <nadawaled87@gmail.com>
2023-11-08 09:02:26 -0800

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @key: key
+ * @ht: hash table
+ * @value: vlaue
+ * Return: 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int idx;
+	char *dub;
+
+	if (!key)
+		return (0);
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
+	idx = hash_djb2((const unsigned char *)key) % ht->size;
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+
+	if (!node->key || !node->value)
+		return (0);
+
+	strcpy(node->key, key);
+	dub = strdup(value);
+	strcpy(node->value, dub);
+
+	if (ht->array[idx])
+	{
+		ht->array[0] = node;
+	}
+	else
+	{
+		ht->array[idx] = node;
+	}
+	return (1);
+}
