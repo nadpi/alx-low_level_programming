fe54a41853850a5597257296bb3792fdf5c42465
nadpi <nadawaled87@gmail.com>
2023-11-08 13:32:58 -0800

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			free(node->key);
+			free(node->value);
+			temp = node;
+			free(temp);
+			node = node->next;
+		}
+	}
+	free(ht);
+}
