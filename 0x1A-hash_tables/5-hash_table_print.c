78b155227002368e99e772ab2bd7b82fe33c4bdc
nadpi <nadawaled87@gmail.com>
2023-11-08 13:19:19 -0800

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,26 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
-	int i;
+	unsigned long int i, flag = 0;
 
-
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			node = ht->array[i];
+		if (node && flag == 1)
+			printf(", ");
+		while (node)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (node != NULL)
+				printf(", ");
+		}
+		flag = 1;
+		}
+	}
+	printf("}");
+	printf("\n");
 }
