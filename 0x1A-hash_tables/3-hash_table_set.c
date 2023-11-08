0e4045ccd465e3f652e6e2edd7d0f8bc2d5d5b98
nadpi <nadawaled87@gmail.com>
2023-11-08 09:38:54 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	char *dub;
 
-	if (!key)
+	if (!key || key[0] == '\0')
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
