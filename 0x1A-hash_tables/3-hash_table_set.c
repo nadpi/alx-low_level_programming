f9d34925310c2714a3a198eed286f83e9715d847
nadpi <nadawaled87@gmail.com>
2023-11-08 09:43:49 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,11 +32,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[idx])
 	{
+		node->next = ht->array[0];
 		ht->array[0]->next = node;
 	}
 	else
 	{
-		node->next = ht->array[idx];
 		ht->array[idx] = node;
 	}
 	return (1);
