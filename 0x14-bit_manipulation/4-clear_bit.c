453c6d682484b2141beb61c453b12cf9f8157615
nada <nadadwaled87@gmail.com>
2024-02-08 17:28:15 -0800

    [4. 100]

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number
+ * @index: index
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	if (index > 64)
+		return (-1);
+	mask = mask << index;
+	*n = *n & ~mask;
+	return (1);
+}
