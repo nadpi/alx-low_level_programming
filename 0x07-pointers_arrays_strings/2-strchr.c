57b50cc2a38b9ee059ddd759ba6d05d23a26aca7
nada <nadadwaled87@gmail.com>
2024-03-13 14:10:20 -0700

    [2. strchr]

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: string
+ * @c: the character
+ * Return: a pointer to the first occurrence of the character c
+ *  in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
