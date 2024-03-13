d59f72d8f2070fc725091beea13030fe9c7d4d5f
nada <nadadwaled87@gmail.com>
2024-03-13 16:16:07 -0700

    [4. strpbrk]

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ * @s: string
+ * @accept: string
+ * Return: a pointer to the byte in s that matches one
+ * of the bytes in accept, or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int min = 0, i, j, len = 0, k = 0;
+
+	while(s[k] != '\0')
+	{
+		len++;
+		k++;
+	}
+	
+	min = len;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				if (j <= min)
+				{
+					min = j;
+					break;
+
+				}
+			}
+		}
+	}
+	return(s + min);
+}
