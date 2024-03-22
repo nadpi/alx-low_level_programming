668f43633be09de529d279664f5e9d683b22d6ea
nada <nadadwaled87@gmail.com>
2024-03-21 18:52:56 -0700

    [7. Insert at index]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = newNode;
 		return (newNode);
 	}
+	if (idx == 0)
+	{
+		newNode->next = *h;
+		*h = newNode;
+		return (newNode);
+	}
 	while (cntnode)
 	{
 		cnt++;
