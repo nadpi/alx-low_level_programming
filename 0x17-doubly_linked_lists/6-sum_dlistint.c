dd3ee5dd6a5f5b22ad6967411ed20fac6113b8fe
nada <nadadwaled87@gmail.com>
2024-03-21 18:26:41 -0700

    [6. Sum list]

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - a function that returns the sum
+ * of all the data (n) of a dlistint_t linked list.
+ * @head: head
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t * curr = head;
+
+	while (curr)
+	{
+		sum += curr->n;
+		curr = curr->next;
+	}
+	return (sum);
+}
