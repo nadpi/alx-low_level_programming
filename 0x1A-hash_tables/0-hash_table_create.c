1b3c6be79716ac6f7007e93ad548ca7f6157c566
nada <nadadwaled87@gmail.com>
2024-08-10 11:17:23 -0700

    [0. >>> ht = {}]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
+	if (!table)
+		return (NULL);
 	table->size = size;
+	table->array = malloc(sizeof(hash_table_t *) * size);
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
 	return (table);
+
 
 }
