d62592f4da6e4587ce46ce1d4bb25c9fd4c70921
nada <nadadwaled87@gmail.com>
2024-08-10 11:19:36 -0700

    [0. >>> ht = {}]

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -14,11 +13,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
-
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -29,8 +27,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
